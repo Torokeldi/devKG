108edbca168beb90336b68abcaaed3fc43d3fbfd
Torokeldi <146943202+Torokeldi@users.noreply.github.com>
2024-08-23 14:48:33 +0600

    update organization item

--- src/components/OrganizationItem/OrganizationItem.tsx
+++ src/components/OrganizationItem/OrganizationItem.tsx
@@ -3,73 +3,72 @@ import React, { useState, useEffect } from "react";
 import useFetch from "../../Hooks/useFetch";
 import { Job } from "../../Hooks/types";
 import "./OrganizationItem.css";
+import { useNavigate } from "react-router-dom";
 
 const OrganizationItem: React.FC = () => {
-    const { data, isLoading } = useFetch({
-        url: "http://3.38.98.134/organizations",
-    });
-    const [companyNames, setCompanyNames] = useState<string[]>([]);
+  const { data, isLoading } = useFetch({
+    url: "http://3.38.98.134/organizations",
+  });
+  const navigate = useNavigate();
+  const [companyNames, setCompanyNames] = useState<string[]>([]);
 
-    useEffect(() => {
-        if (Array.isArray(data) && data.length > 0) {
-            const names = data.map(
-                (job: Job) => job.organization_name || "Не указано"
-            );
-            setCompanyNames(names);
-        }
-    }, [data]);
+  const defaultIcon =
+    "https://devkg.com/images/organizations/733525f43fb53ec1ff2912d7365232dd.png";
 
-    if (isLoading) {
-        return <div>Loading...</div>;
+  useEffect(() => {
+    if (Array.isArray(data) && data.length > 0) {
+      const names = data.map(
+        (job: Job) => job.organization_name || "Не указано"
+      );
+      setCompanyNames(names);
     }
+  }, [data]);
 
-    return (
-        <div>
-            {Array.isArray(data) &&
-                data.map((job: Job) => (
-                    <div 
-                        className="organization__item" 
-                        key={job.id} 
-                    >
-                        <div className="organizationItem_left">
-                            <div className="organization_logo">
-                                <img 
-                                    src={job.icon} 
-                                    alt="Organization Logo" 
-                                />
-                            </div>
-                            <div className="organization_details">
-                                <b className="organization__title">Компания</b>
-                                <b className="organization__name">
-                                    {job.organization_name || "Не указано"}
-                                </b>
-                            </div>
-                        </div>
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
-                        <div className="organizationItem_right">
-                            <div className="organization_details">
-                                <b className="organization__title">Вакансий: </b>
-                                <b className="organization__num">
-                                    {job.jobs_count || 0}
-                                </b>
-                            </div>
-                            <div className="organization_details">
-                                <b className="organization__title">Мероприятий: </b>
-                                <b className="organization__num">
-                                    {job.events_count || 0}
-                                </b>
-                            </div>
-                            <div className="organization_details">
-                                <b className="organization__title">Видео: </b>
-                                <b className="organization__num">
-                                    {job.meetups_count || 0}
-                                </b>
-                            </div>
-                        </div>
-                    </div>
-                ))}
-        </div>
-    );
+  const handleClick = (id: string) => {
+    navigate(`/organizations/${id}`);
+  };
+
+  return (
+    <div>
+      {Array.isArray(data) &&
+        data.map((job: Job) => (
+          <div
+            className="organization__item"
+            key={job.id}
+            onClick={() => handleClick(job.id)}
+          >
+            <div className="organizationItem_left">
+              <div className="organization_logo">
+                <img src={job.icon || defaultIcon} alt="" />
+              </div>
+              <div className="organization_details">
+                <b className="organization__title">Компания</b>
+                <b className="organization__name">{job.name || "Не указано"}</b>
+              </div>
+            </div>
+
+            <div className="organizationItem_right">
+              <div className="organization_details">
+                <b className="organization__title">Вакансий: </b>
+                <b className="organization__num">{job.jobs_count || 0}</b>
+              </div>
+              <div className="organization_details">
+                <b className="organization__title">Мероприятий: </b>
+                <b className="organization__num">{job.events_count || 0}</b>
+              </div>
+              <div className="organization_details">
+                <b className="organization__title">Видео: </b>
+                <b className="organization__num">{job.meetups_count || 0}</b>
+              </div>
+            </div>
+          </div>
+        ))}
+    </div>
+  );
 };
 
 export default OrganizationItem;
